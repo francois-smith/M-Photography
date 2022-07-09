3a12e415fce5446409026c3cb89150b0918e7ab9
Francois Smith <steamfaffa@gmail.com>
2022-07-09 23:54:55 +0200

    Changed Colour Scheme

--- src/themes/default.jsx
+++ src/themes/default.jsx
@@ -1,17 +1,19 @@
 export default {
     colors: {
-        primary: "#697184",
-        accent: "#b1a6a4",
-        background1: "#d8cfd0",
-        background2: "#413f3d",
-        text: "#f2f1ef"
+        offwhite: "#d8cfd0",
+        darkgray: "#413f3d",
+        brown: "#4b4233",
+        browngray: "#989189",
+        silver: "#a7adad",
+        chestnut: "#dbbab1",
+        rosegold: "#c18086"
     },
     breakpoints: {
         xsm: 'screen and (max-width: 320px)',
         sm: 'screen and (max-width: 568px)',
         md: 'screen and (max-width: 800px)',
         lg: 'screen and (max-width: 1024px)',
-        xl: 'screen and (max-width: 1280px)',
+        xl: 'screen and (max-width: 1300px)',
         xxl: 'screen and (max-width: 1440px)',
     },
 }
