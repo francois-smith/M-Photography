1e7b5a4ae6d3f00faf9017539409ba6e263add8e
Francois Smith <steamfaffa@gmail.com>
2022-07-09 23:54:32 +0200

    Hide Burger Menu When Scrolling

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+
+let lastScroll = window.scrollY;
+window.onscroll = function() {
+	if(window.scrollY > 50) {
+		document.querySelector(".bm-burger-button").style.opacity = "0";
+	}
+	else {
+		document.querySelector(".bm-burger-button").style.opacity = "1";
+	}
+	lastScroll = window.scrollY;
+} 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<App/>
 );
 
