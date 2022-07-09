e8ce12640f85ad5ba706bf95e0b216af884d159a
Francois Smith <steamfaffa@gmail.com>
2022-07-09 23:54:43 +0200

    Added Footer (Done)

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import {FooterContainer} from "./footer-styles"
+import FLogo from "../media/svgs/f-logo.svg"
+import {AiOutlineInstagram, AiFillFacebook} from "react-icons/ai"
+
+const style = { color: "#413f3d", fontSize: "2.5em" }
 
 function Footer() {
-  return (
-    <div className="Footer">
-      <footer>
-        <div >
-          <p>
-            Copyright &copy; Your Website 2020
-          </p>
-        </div>
-      </footer>
-    </div>
-  );
+	return (
+		<FooterContainer>
+			<footer>
+				<div className="footer-divider"/>
+				<div className="footer-content">
+					<p id="copyright-notice">
+						&copy;2016-{new Date().getFullYear()} Maryna M Photography
+					</p>
+					<div className="socials-container">
+						<a href="https://www.facebook.com/marynamphotography/"><AiFillFacebook style={style}/></a>
+						<a href="https://www.instagram.com/mphotography_maryna/"><AiOutlineInstagram style={style}/></a>
+					</div>
+					<div className="f-logo-container">
+						<a href="https://francois-smith.com"><img id="f-logo" src={FLogo} alt="Francois Smith Logo" /></a>
+					</div>
+				</div>
+			</footer>
+		</FooterContainer>
+	);
 }
 
 export default Footer;
