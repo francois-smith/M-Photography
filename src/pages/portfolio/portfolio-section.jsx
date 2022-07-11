63323655f6c335960f4d920459911660d0760cbb
Francois Smith <steamfaffa@gmail.com>
2022-07-11 11:17:46 +0200

    Reactive Image Gallery (working)

--- src/pages/portfolio/portfolio-section.jsx
+++ src/pages/portfolio/portfolio-section.jsx
@@ -1,55 +1,67 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-
-const sections = [
-	{ name: `Portfolio`},
-	{
-		name: `Vennila`,
-	},
-	{
-		name: `Afrin`,
-	},
-]
-
-/* <div class="filters-container">
-                        <div onclick="filterImages(this)" class="image-filter"></div>
-                        <div onclick="filterImages(this)" class="image-filter">Newborn</div>
-                        <div onclick="filterImages(this)" class="image-filter">Milkbath/Cakesmash</div>
-                    </div>
-                </div>
-                <div id="studio-container">
-                    <div class="images-container">
-                        
-                    </div>
-                </div>
-            </div>
-            <div class="section-content" data-section="loc">
-                <div class="section-header">
-                    <h2>On Location Images</h2>
-                    <div class="filters-container">
-                        <div onclick="filterImages(this)" class="image-filter">Wedding</div>
-                        <div onclick="filterImages(this)" class="image-filter">Couples & Engagement</div>
-                        <div onclick="filterImages(this)" class="image-filter">Family</div>
-                        <div onclick="filterImages(this)" class="image-filter">Graduation</div>
-                        <div onclick="filterImages(this)" class="image-filter">Maternity</div>
-                        <div onclick="filterImages(this)" class="image-filter">Matric Farewell</div>
-                        <div onclick="filterImages(this)" class="image-filter">Mommy & Me</div>
-                        <div onclick="filterImages(this)" class="image-filter">Portrait Portfolio</div>
-                    </div> */
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Gallery from 'react-grid-gallery';
+import { GalleryContainer } from "./portfolio-styles"
 
 function Portfolio() {
-	return (
-		<body>
-			<h3>Portfolio Page</h3>
-			{
-				sections.map((user, index) => (
-					<h5 key={index}>
+    useEffect(() => {
+		document.title = "M Photography | Portfolio";
+	}, []);
 
-					</h5>
-				))
-			}
-		</body>
-	);
+	const { type } = useParams();
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        let url = 'https://maryna-m-photography.com/db.php';
+        var params = [];
+        params.push(`section=${cleanRequest(type)}`);
+
+        fetch(url, { method: "POST", headers: {'Content-Type': 'application/x-www-form-urlencoded;'}, body: params })
+        .then(async response => {
+            let data = await response.json();
+
+            for(let image of data.data){
+                image.src = "/uploads/"+image.src;
+                image.thumbnail = image.src;
+            }
+
+            shuffle(data.data);
+
+            setImages(data.data);
+        });
+    }, []);
+
+    const shuffle = (array) => {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    };
+
+    function cleanRequest(type){
+        switch(type){
+            case "couples+&+engagement": return "loc-couple";
+            case "family": return "loc-family";
+            case "graduation": return "loc-graduation";
+            case "maternity": return "loc-maternity";
+            case "matric+farewell": return "loc-matric";
+            case "mommy+&+me": return "loc-mommy";
+            case "portrait+portfolio": return "loc-portfolio";
+            case "newborn": return "studio-newborn";
+            case "milkbath": return "studio-milkbath";
+            case "cakesmash": return "studio-cakesmash";
+            case "portfolio": return "studio-portfolio";
+            case "baby+milestone": return "studio-milestone";
+        }
+    }
+
+    return (
+        <GalleryContainer>
+            <Gallery images={images} rowHeight={350}/>
+        </GalleryContainer>
+    );
 }
 
 export default Portfolio;
