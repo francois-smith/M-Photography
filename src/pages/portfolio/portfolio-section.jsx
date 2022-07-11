84dc14214e2c79c5f0371f4e065dd13f704e1b1e
Francois Smith <steamfaffa@gmail.com>
2022-07-11 22:17:33 +0200

    Dynamic Cards And Sections

--- src/pages/portfolio/portfolio-section.jsx
+++ src/pages/portfolio/portfolio-section.jsx
@@ -1,67 +1,121 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import { render } from "react-dom";
 import { useParams } from "react-router-dom";
-import Gallery from 'react-grid-gallery';
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 import { GalleryContainer } from "./portfolio-styles"
 
-function Portfolio() {
-    useEffect(() => {
+function Portfolio() {    
+	const { type } = useParams();
+		const [photos, setPhotos] = useState([]);
+		const [currentImage, setCurrentImage] = useState(0);
+		const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+		useEffect(() => {
 		document.title = "M Photography | Portfolio";
+			getImages();
 	}, []);
 
-	const { type } = useParams();
-    const [images, setImages] = useState([]);
+	const openLightbox = useCallback((event, { photo, index }) => {
+		setCurrentImage(index);
+		setViewerIsOpen(true);
+	}, []);
+	
+	const closeLightbox = () => {
+		setCurrentImage(0);
+		setViewerIsOpen(false);
+	};
 
-    useEffect(() => {
-        let url = 'https://maryna-m-photography.com/db.php';
-        var params = [];
-        params.push(`section=${cleanRequest(type)}`);
+	async function getImages(){
+		let url = 'https://maryna-m-photography.com/db.php';
+		var params = [];
+		params.push(`section=${cleanRequest(type)}`);
 
-        fetch(url, { method: "POST", headers: {'Content-Type': 'application/x-www-form-urlencoded;'}, body: params })
-        .then(async response => {
-            let data = await response.json();
+		const response = await fetch(url, { method: "POST", headers: {'Content-Type': 'application/x-www-form-urlencoded;'}, body: params })
+		let data = await response.json();
+		
+		let returnArray = [];
+		for(let image of data.data){
+			await addImage(image, returnArray);
+		}
 
-            for(let image of data.data){
-                image.src = "/uploads/"+image.src;
-                image.thumbnail = image.src;
-            }
+		shuffle(returnArray);
+		setPhotos(returnArray);
+	}
 
-            shuffle(data.data);
+	function addImage(image, array) {
+		return new Promise((resolve, reject) => {
+			const img = new Image();
+			img.src = "/uploads/"+image.src;
 
-            setImages(data.data);
-        });
-    }, []);
+			img.onload = () => {
+				let photo = {
+						src: "/uploads/"+image.src,
+						width: img.width,
+						height: img.height,
+				}
+				array.push(photo);
+				resolve();
+			};
+		})
+	}
 
-    const shuffle = (array) => {
-        for (var i = array.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-    };
+	const shuffle = (array) => {
+		for (var i = array.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	};
 
-    function cleanRequest(type){
-        switch(type){
-            case "couples+&+engagement": return "loc-couple";
-            case "family": return "loc-family";
-            case "graduation": return "loc-graduation";
-            case "maternity": return "loc-maternity";
-            case "matric+farewell": return "loc-matric";
-            case "mommy+&+me": return "loc-mommy";
-            case "portrait+portfolio": return "loc-portfolio";
-            case "newborn": return "studio-newborn";
-            case "milkbath": return "studio-milkbath";
-            case "cakesmash": return "studio-cakesmash";
-            case "portfolio": return "studio-portfolio";
-            case "baby+milestone": return "studio-milestone";
-        }
-    }
+	function cleanRequest(type){
+		switch(type){
+			case "couples+&+engagement": return "loc-couple";
+			case "family": return "loc-family";
+			case "graduation": return "loc-graduation";
+			case "maternity": return "loc-maternity";
+			case "matric+farewell": return "loc-matric";
+			case "mommy+&+me": return "loc-mommy";
+			case "portrait+portfolio": return "loc-portfolio";
+			case "newborn": return "studio-newborn";
+			case "milkbath": return "studio-milkbath";
+			case "cakesmash": return "studio-cakesmash";
+			case "portfolio": return "studio-portfolio";
+			case "baby+milestone": return "studio-milestone";
+		}
+	}
 
-    return (
-        <GalleryContainer>
-            <Gallery images={images} rowHeight={350}/>
-        </GalleryContainer>
-    );
+
+	return (
+		<div>
+			{
+			photos.length !== 0 ? (
+			<GalleryContainer>
+				<Gallery margin={5} direction={"column"} photos={photos} onClick={openLightbox} />
+				<ModalGateway>
+						{viewerIsOpen ? (
+							<Modal onClose={closeLightbox}>
+								<Carousel
+								currentIndex={currentImage}
+								views={photos.map((x) => ({
+									...x,
+									srcset: x.srcSet,
+									caption: x.title
+								}))}
+								/>
+							</Modal>
+						) : null}
+				</ModalGateway>
+			</GalleryContainer>
+			) 
+			:
+			(
+				<GalleryContainer></GalleryContainer>
+			)
+			}
+		</div>
+	);
 }
 
 export default Portfolio;
