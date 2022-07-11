4a36248e2e3f5828eacb996133ce7ebf2d12fc02
Francois Smith <steamfaffa@gmail.com>
2022-07-11 22:15:39 +0200

    Added Global Style Rules

--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -3,6 +3,10 @@ import { normalize } from "styled-normalize";
 
 const GlobalStyles = createGlobalStyle`
     ${normalize};
+    :root {
+        --toastify-color-light: ${props => props.theme.colors.offwhite} !important;
+        --toastify-text-color-light: ${props => props.theme.colors.darkgray} !important;
+    }
     * {
         margin: 0;
         padding: 0;
@@ -12,16 +16,35 @@ const GlobalStyles = createGlobalStyle`
         scroll-behavior: smooth;
         overflow-x: hidden;
         position: absolute;
-        width: 100%;
+        width: 100%;  
     }
     body {
         background-color: ${props => props.theme.colors.offwhite};
         font-family: 'Nunito', sans-serif;
         font-weight: 400;
     }
-    .about-body, .contact-body{
+    .about-body{
         display: flex;
         justify-content: center;
+    }
+    .contact-body{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .divider-container{
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .divider-image-container{
+        display: flex;
+        justify-content: center;
+        overflow: hidden;
+        opacity: 0.8;
+
+        img{
+            height: 20px;
+        }
     }
 `;
 
